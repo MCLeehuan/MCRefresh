c28251b131abf195d84667f2533ecd9915559c86
MCLee <120710460@qq.com>
2015-03-15 20:47:30 +0800

    Initial Commit

---
+++ MCRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MCRefresh
+//
+//  Created by MC Lee on 15/3/15.
+//  Copyright (c) 2015年 GoldenVision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MCRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MCRefresh
+//
+//  Created by MC Lee on 15/3/15.
+//  Copyright (c) 2015年 GoldenVision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
